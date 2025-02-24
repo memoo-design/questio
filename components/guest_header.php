8cf850ff8ebd3efa943bb08c05a8b43be3f62d2f
memoo-design <memoonawaheed8@gmail.com>
2025-02-24 10:38:20 +0500

    updated nav bar

--- components/guest_header.php
+++ components/guest_header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
     <div class="container">
-        <a class="navbar-brand" href="#">Questio</a>
+        <a class="navbar-brand" href="#index.php">Questio</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,8 +8,10 @@
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#services">Our Services</a></li>
-                <li class="nav-item"><a class="nav-link" href="#testimonials">Testimonials</a></li>
+                <li class="nav-item"><a class="nav-link" href="#testimonial">Testimonials</a></li>
                 <li class="nav-item"><a class="nav-link" href="#about">About Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="#Contact">Contact Us</a></li>
+                
                 <li class="nav-item">
                     <a class="btn btn-primary sign-in-btn" href="../questio/pages/guest/login.php">Login In</a>
                 </li>
